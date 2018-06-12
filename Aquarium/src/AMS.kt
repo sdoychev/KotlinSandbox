89e29d96adbfb393918ff1dbae803925c7614402
Stefan Doychev <sdoychev@gmail.com>
2018-06-12 19:23:23 +0300

    Lesson 3.11

--- Aquarium/src/AMS.kt
+++ Aquarium/src/AMS.kt
@@ -8,6 +8,10 @@ fun feedFish() {
     val day = getRandomDay()
     val food = getFishFood(day)
     println("Today is $day and the fish are fed with $food")
+
+    if (shouldChangeWater(day)) {
+        println("Change the water today!")
+    }
 }
 
 fun getRandomDay(): String {
@@ -26,4 +30,12 @@ fun getFishFood(day: String): String {
         "Sunday" -> "Suns"
         else -> "Defaulty"
     }
+}
+
+fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return true
+}
+
+fun canAddFish(tankSize: Double, currentFish: List<Int>, fishSize: Int = 2, hasDecorations: Boolean = true): Boolean {
+    return (tankSize * if (hasDecorations) 0.8 else 1.0) >= (currentFish.sum() + fishSize)
 }
