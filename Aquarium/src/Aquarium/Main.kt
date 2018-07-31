dcaf5806b22488b6463fdbfb7e3eca64ba55843e
Stefan Doychev <sdoychev@gmail.com>
2018-07-31 15:03:48 +0300

    Final lesson
    Wrap up course

--- Aquarium/src/Aquarium/Main.kt
+++ Aquarium/src/Aquarium/Main.kt
@@ -5,6 +5,23 @@ fun main(args: Array<String>) {
     buildAquarium()
     makeFish()
 
+    highOrderFunction()
+}
+
+fun highOrderFunction() {
+    val numbers = listOf<Int>(1,2,3,4,5,6,7,8,9,0)
+    print(numbers.divisibleBy { it.rem(3) })
+
+}
+
+fun List<Int>.divisibleBy(block: (Int) -> Int): List<Int> {
+    val result = mutableListOf<Int>()
+    for (item in this) {
+        if (block(item) == 0) {
+            result.add(item)
+        }
+    }
+    return result
 }
 
 fun buildAquarium() {
